dcd212251635f8dc5c971d1ea59076e85e2eea1c
Vladyslav Radchenko <vladcommunication@gmail.com>
2022-12-15 19:48:34 +0100

    Init composer basick packages , basic folder structure

---
+++ food-app.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * @link              https://github.com/sweazy4a/food-app
+ * @since             1.1.9
+ * @package           Food_App
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Food App Plugin
+ * Plugin URI:        https://github.com/sweazy4a/food-app
+ * Description:       Custom invoices features
+ * Version:           1.1.9
+ * Author:            Vlad
+ * Plugin URI:        https://github.com/sweazy4a/food-app
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       food_app
+ 
+ */
