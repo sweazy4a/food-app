4dee75598c0a421dba0fc5322ad107b25b3707e7
Vladyslav Radchenko <vladcommunication@gmail.com>
2022-12-15 20:32:13 +0100

    generate orders json , initiate Init class

--- includes/FA_Init.php
+++ includes/FA_Init.php
@@ -1,1 +1,35 @@
 <?php
+
+namespace App\FoodApp;
+// use App\FoodApp\CPT\FA_Invoice;
+// use App\FoodApp\Templates\FA_Templates;
+// use App\FoodApp\Ajax\FA_RegisterAjax;
+
+if (!defined('ABSPATH')) {
+    die;
+}
+
+
+/**
+ * Class FA_Init
+ */
+final class FA_Init
+{
+    private static function init(): array
+    {
+       return [
+           //Utils
+        //    FA_Invoice::class,
+        //    FA_Templates::class,
+        //    FA_RegisterAjax::class,
+       ];
+    }
+
+    public static function register() : void
+    {
+        foreach (self::init() as $class)
+        {
+            new $class();
+        }
+    }
+}
